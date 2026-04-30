bc407cda9d37dfbeaee9355cc06eae78cc30fb8d
nikolariorovic <nikolariorovic@hotmail.com>
2026-04-30 18:15:36 +0200

    --Admin feature login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Session\TokenMismatchException;
-use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,13 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (Throwable $e, $request) {
-            Log::info('exception render hook', [
-                'class' => get_class($e),
-                'path' => $request->path(),
-                'is_admin' => $request->is('admin/*'),
-            ]);
+            $is419 = $e instanceof TokenMismatchException
+                || ($e instanceof HttpExceptionInterface && $e->getStatusCode() === 419);
 
-            if ($e instanceof TokenMismatchException && $request->is('admin/*')) {
+            if ($is419 && $request->is('admin/*')) {
                 return redirect()->route('admin.login.show')
                     ->with('error', 'Sesija je istekla. Molimo prijavite se ponovo.');
             }
