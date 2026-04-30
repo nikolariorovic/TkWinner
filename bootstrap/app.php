e739383748ea32390377eb3d34f47306a20f0bc1
nikolariorovic <nikolariorovic@hotmail.com>
2026-04-30 18:08:13 +0200

    --Admin feature login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Session\TokenMismatchException;
+use Illuminate\Support\Facades\Log;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,8 +16,14 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->redirectGuestsTo(fn () => route('admin.login.show'));
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->render(function (TokenMismatchException $e, $request) {
-            if ($request->is('admin/*')) {
+        $exceptions->render(function (Throwable $e, $request) {
+            Log::info('exception render hook', [
+                'class' => get_class($e),
+                'path' => $request->path(),
+                'is_admin' => $request->is('admin/*'),
+            ]);
+
+            if ($e instanceof TokenMismatchException && $request->is('admin/*')) {
                 return redirect()->route('admin.login.show')
                     ->with('error', 'Sesija je istekla. Molimo prijavite se ponovo.');
             }
